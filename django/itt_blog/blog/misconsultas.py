b919cc2cb95c72c8681974bfa1620d9583bf3a3a
KeniaP <perezkeniaa@gmail.com>
2017-09-27 11:45:28 -0700

    Final

--- django/itt_blog/blog/misconsultas.py
+++ django/itt_blog/blog/misconsultas.py
@@ -1,3 +1,12 @@
+#Creacion de objetos
+>>> from blog.models import Blog
+>>> b = Blog(name='Beatles Blog', tagline='All the latest Beatles news.')
+>>> b.save()
+
+ #guardar cambios en los objetos
+>>> b5.name = 'New name'
+>>> b5.save()
+
 # este proceso imporca la importacion de las clases y su objetivo principal es el de asignar llave foranea y almacenar el id del campo creado para la llamaforanea
 >>> from blog.models import Blog, Entry, Author
 >>> ed = Entry.objects.get(pk=1)
@@ -13,7 +22,7 @@
 ...  ).filter(
 ...    pub_date__gte=datetime(2005, 1, 30)
 
-
+#Recuperacion de objetos
 >>> Blog.objects.filter(
 ...    headline__startswith='What'
 ... ).exclude(
@@ -22,7 +31,7 @@
 ...    pub_date__gte=datetime(2005, 1, 30)
 ... )
 
-
+#filtrado de quierYSets unicos
 >>> q1 = Author.objects.filter(headline__startswith="What")
 >>> q2 = q1.exclude(pub_date__gte=datetime.date.today())
 >>> q3 = q1.filter(pub_date__gte=datetime.date.today())
@@ -36,6 +45,7 @@
 >>> q = q.exclude(body_text__icontains="food")
 >>> print(q)
 
+#Limitacion de querysets
 >>> Author.objects.all()[:5]
 >>> Blog.objects.all()[:5]
 >>> Author.objects.all()[5:10]
@@ -47,6 +57,7 @@
 >>> Author.objects.order_by('headline')[0:1].get()
 >>> Blog.objects.order_by('headline')[0:1].get()
 
+#Busqueda de campo
 >>> Author.objects.filter(pub_date__lte='2006-01-01')
 >>> Blog.objects.filter(pub_date__lte='2006-01-01')
 >>> Author.objects.filter(blog_id=4)
@@ -69,7 +80,7 @@
 >>> Blog.objects.filter(entry__authors__name__isnull=True)
 >>> Blog.objects.filter(entry__authors__isnull=False, entry__authors__name__isnull=True)
 
-
+#intercambiando relaciones de valores multiples
 >>>Blog.objects.filter(entry__headline__contains='Lennon', >>>entry__pub_date__year=2008)
 >>>Blog.objects.filter(entry__headline__contains='Lennon').filter(entry__pub_date__year=2008)
 
@@ -88,7 +99,7 @@
 >>> Blog.objects.filter(mod_date__gt=F('pub_date') + timedelta(days=3))
 >>> F('somefield').bitand(16)
 
-
+#El acceso directo de busqueda de pk
 >>> Blog.objects.get(id__exact=14) # Explicit form
 >>> Blog.objects.get(id=14) # __exact is implied
 >>> Blog.objects.get(pk=14) # pk implies id__exact
@@ -106,13 +117,14 @@
 >>> Author.objects.filter(headline__contains='%')
 >>> Blog.objects.filter(headline__contains='%')
 
-
+#almacenamiento en cache y query sets
 >>> queryset = Author.objects.all()
 >>> queryset = Blog.objects.all()
 >>> print([p.headline for p in queryset]) # Evaluate the query set.
 >>> print([p.pub_date for p in queryset]) # Re-use the cache from the evalua
 
 
+#cuando querysets no se almacenan en cache
 >>> queryset = Author.objects.all()
 >>> queryset = Blog.objects.all()
 >>> print(queryset[5]) # Queries the database
@@ -129,6 +141,8 @@
 >>> Blog in queryset
 >>> list(queryset)
 
+
+#utilizar un gestor inverso personalizado
 from django.db import models
 class Entry(models.Model):
     #...
@@ -140,3 +154,12 @@ b.entry_set(manager='entries').all()
 b.entry_set(manager='entries').is_published()
 b = Blog.objects.get(id=1)
 b.entry_set.set([e1, e2])
+
+
+# consulta sobre objetos relacionados
+Entry.objects.filter(blog=b) # Query using object instance
+Entry.objects.filter(blog=b.id) # Query using id from instance
+Entry.objects.filter(blog=5) # Query using id directly
+
+
+---------------------##Referencia https://docs.djangoproject.com/en/1.11/topics/db/queries/ ## ----------------------------------------
